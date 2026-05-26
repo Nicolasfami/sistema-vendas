0061541b50d886669bc302b5047b32204a1acb76
Nicolasfami <nicolasfami2022@gmail.com>
2026-05-26 18:31:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1070,6 +1070,15 @@ def icone_svg(nome):
             <path d="M19 9l2-2-2-2"/>
             <path d="M5 15l-2 2 2 2"/>
         </svg>
+        """,
+        "ranking": """
+        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+            <path d="M6 9H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h2"/>
+            <path d="M18 9h2a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-2"/>
+            <path d="M6 3h12v10a6 6 0 0 1-12 0V3z"/>
+            <path d="M12 19v3"/>
+            <path d="M8 22h8"/>
+        </svg>
         """
     }
     return icones.get(nome, "")
